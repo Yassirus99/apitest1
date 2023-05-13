0b9ebf434ba3fa09139163b8ab2459cc3aaedffc
yassirus99 <yassir6599@gmail.com>
2023-05-12 18:50:29 -0600

    Conexion con BD exitosa 2

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,9 +3,9 @@ class AuthenticationController < ApplicationController
   skip_before_action :authenticate_request, only: [:authenticate]
 
   def authenticate
-    user = Usuario.find_by(email: params[:email])
+    user = Usuario.find_by(CORREO_ELECTRONICO_USUARIO: params[:email])
     if user && user.authenticate(params[:password])
-      token = JsonwebToken.encode(user_id: user.id)
+      token = JWT.encode({ user_id: user.id }, Rails.application.secret_key_base)
       render json: { token: token }, status: :ok
     else
       render json: { error: 'Correo electrónico o contraseña incorrectos' }, status: :unauthorized
@@ -18,6 +18,7 @@ class AuthenticationController < ApplicationController
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
+end
 end
 
   /
@@ -32,4 +33,4 @@ end
     end
   end/
 
-end
+
