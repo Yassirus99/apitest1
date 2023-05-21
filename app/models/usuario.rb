8e2ee2940a4eb8d39434dced22e0108a91a2d308
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2023-05-20 23:26:51 -0600

    Integrar Swagger

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,19 +1,18 @@
 class Usuario < ApplicationRecord
   self.table_name = "USUARIO"
   
-    # Columnas de la tabla usuario
-    # Debes asegurarte que los nombres de las columnas coincidan exactamente con los de tu base de datos
-    # y especificar el tipo de datos correspondiente
-    attribute :id_usuario, :integer
-    attribute :id_empleado, :integer
-    attribute :id_f_tipo_usuario, :integer
-    attribute :id_f_profesor, :integer
-    attribute :nombre_usuario, :string
-    attribute :correo_electronico_usuario, :string
-    attribute :descripcion_rol, :string
-    attribute :contrasenia, :string
-    attribute :activo_usuario, :boolean
-    attribute :fecha_creacion_usuario, :datetime
-    attribute :fecha_modificacion_usuario, :datetime
-  end
-  
+  # Columnas de la tabla usuario
+  # Debes asegurarte que los nombres de las columnas coincidan exactamente con los de tu base de datos
+  # y especificar el tipo de datos correspondiente
+  attribute :id_usuario, :integer
+  attribute :id_empleado, :integer
+  attribute :id_f_tipo_usuario, :integer
+  attribute :id_f_profesor, :integer
+  attribute :nombre_usuario, :string
+  attribute :correo_electronico_usuario, :string
+  attribute :descripcion_rol, :string
+  attribute :contrasenia, :string
+  attribute :activo_usuario, :boolean
+  attribute :fecha_creacion_usuario, :datetime
+  attribute :fecha_modificacion_usuario, :datetime
+end
